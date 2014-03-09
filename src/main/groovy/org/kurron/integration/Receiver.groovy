97d0a0f296c9646295afa3c1073e45672fdeed6d
Ron Kurr <jtkirk@gmail.com>
2014-03-08 20:06:01 -0500

    The Spring Integration end-point now writes to MongoDB.

--- src/main/groovy/org/kurron/integration/Receiver.groovy
+++ src/main/groovy/org/kurron/integration/Receiver.groovy
@@ -2,13 +2,23 @@ package org.kurron.integration
 
 import groovy.util.logging.Slf4j
 import org.kurron.mongodb.Customer
+import org.kurron.mongodb.CustomerRepository
 import org.springframework.integration.annotation.ServiceActivator
 
 @Slf4j
 class Receiver {
+
+
+    private final CustomerRepository theRepository
+
+    Receiver( CustomerRepository repository ) {
+        theRepository = repository
+    }
+
     @SuppressWarnings( ["GroovyUnusedDeclaration", "GrMethodMayBeStatic"] )
     @ServiceActivator
     void persistCustomer( Customer customer) {
-        log.debug( "{}", customer )
+        def persisted = theRepository.save( customer )
+        log.debug( "Just stored {} in MongoDB", persisted )
     }
 }
