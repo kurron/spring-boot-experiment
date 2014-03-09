11e195c9d4b889d1ac2386caa5dd6d4679dc1636
Ron Kurr <jtkirk@gmail.com>
2014-03-08 20:13:07 -0500

    The transormer bean is now autowired.

--- src/main/groovy/org/kurron/integration/StringToCustomerTransformer.groovy
+++ src/main/groovy/org/kurron/integration/StringToCustomerTransformer.groovy
@@ -3,8 +3,10 @@ package org.kurron.integration
 import groovy.util.logging.Slf4j
 import org.kurron.mongodb.Customer
 import org.springframework.integration.annotation.Transformer
+import org.springframework.stereotype.Component
 
 @Slf4j
+@Component
 class StringToCustomerTransformer {
     @SuppressWarnings(["GroovyUnusedDeclaration", "GrMethodMayBeStatic"])
     @Transformer
