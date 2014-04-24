451d849b031cf4fd3140e28ae24ab7e15ba30d63
Ron Kurr <rkurr@transparent.com>
2014-04-24 11:19:15 -0400

    Caching enabled in the configuration.  Still haven't cached anything yet.

--- src/main/groovy/com/transparent/asgard/thor/amqp/AmqpContext.groovy
+++ src/main/groovy/com/transparent/asgard/thor/amqp/AmqpContext.groovy
@@ -1,6 +1,10 @@
 package com.transparent.asgard.thor.amqp
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.cache.Cache
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCache
+import org.springframework.cache.support.SimpleCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.ImportResource
@@ -10,6 +14,7 @@ import org.springframework.scheduling.annotation.ScheduledAnnotationBeanPostProc
 @Configuration
 @ImportResource( 'classpath:META-INF/spring/amqp-context.xml' )
 @EnableConfigurationProperties( CustomAmqpProperties )
+@EnableCaching
 class AmqpContext {
 
     @Bean
@@ -20,5 +25,12 @@ class AmqpContext {
     @Bean
     Sender mySender() {
         new Sender()
+    }
+
+    @Bean
+    SimpleCacheManager cacheManager() {
+        def manager = new SimpleCacheManager()
+        manager.caches = [new ConcurrentMapCache( 'ronbo' )]
+        manager
     }
 }
