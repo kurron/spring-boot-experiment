b589b0da2611889750f04e6ae703f6e7f14948fd
Ron Kurr <rkurr@transparent.com>
2014-04-25 19:22:24 -0400

    Proved we can run from within Tomcat.

--- src/main/groovy/com/transparent/asgard/thor/Application.groovy
+++ src/main/groovy/com/transparent/asgard/thor/Application.groovy
@@ -2,13 +2,26 @@ package com.transparent.asgard.thor
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan
 
 @SuppressWarnings( 'GrMethodMayBeStatic' )
 @EnableAutoConfiguration
 @ComponentScan( ['com.transparent.asgard.thor'] )
-class Application {
+class Application extends SpringBootServletInitializer {
+
+    /**
+     * This gets called when we are running from the command-line.
+     * @param args any arguments to the program.
+     */
     static void main( String[] args ) {
         SpringApplication.run( Application, args )
     }
+
+    @Override
+    protected SpringApplicationBuilder configure( SpringApplicationBuilder application ) {
+        application.sources( Application )
+    }
+
 }
